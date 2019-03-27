98b75304651264f7a1f6a6ba3fbbb66e6ade9d59
Adrian Cucos <dielorg@gmail.com>
2019-03-27 16:18:34 +0100

    added q12

--- src/main/java/Q1.java
+++ src/main/java/Q1.java
@@ -1,17 +1,14 @@
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.*;
 
-import javax.xml.crypto.Data;
-import java.awt.dnd.DropTarget;
-
 public class Q1 {
 
-    private static final String startingpath = "/home/adrian/Documents/TUe/Quartile 3/2ID70 Data intensive systems/M2/tables/";
+    /** MAKE SURE TO CHANGE THIS TO YOUR ACTUAL STARTING PATH**/
+    private static final String STARTING_PATH = "/home/adrian/Documents/TUe/Quartile 3/2ID70 Data intensive systems/M2/tables/";
+    /** Ok, thanks bye  **/
 
     public static void main(String[] args){
         String master = "local[1]";
@@ -32,7 +29,7 @@ public class Q1 {
             new StructField("Description", DataTypes.StringType, true, Metadata.empty()),
             new StructField("TotalECTS", DataTypes.IntegerType, true, Metadata.empty())
         });
-        Dataset<Row> dfDegrees = spark.read().schema(degreeSchema).option("header",true).csv(startingpath + "Degrees.table");
+        Dataset<Row> dfDegrees = spark.read().schema(degreeSchema).option("header",true).csv(STARTING_PATH + "Degrees.table");
 
         StructType studentsSchema = new StructType(new StructField[]{
                 //StudentId, StudentName, Address, BirthyearStudent, Gender
@@ -42,7 +39,7 @@ public class Q1 {
                 new StructField("BirthyearStudent", DataTypes.IntegerType, true, Metadata.empty()),
                 new StructField("Gender", DataTypes.IntegerType, true, Metadata.empty())
         });
-        Dataset<Row> dfStudents = spark.read().schema(studentsSchema).option("header", true).csv(startingpath+"Students.table");
+        Dataset<Row> dfStudents = spark.read().schema(studentsSchema).option("header", true).csv(STARTING_PATH +"Students.table");
 
         StructType studentRegSchema =  new StructType(new StructField[]{
                 //StudentRegistrationId, StudentId, DegreeId, RegistrationYear
@@ -52,7 +49,7 @@ public class Q1 {
                 new StructField("RegistrationYear", DataTypes.IntegerType, true, Metadata.empty()),
         });
         Dataset<Row> dfStudentRegistrations = spark.read().schema(studentRegSchema).option("header", true)
-                .csv(startingpath+"StudentRegistrationsToDegrees.table");
+                .csv(STARTING_PATH +"StudentRegistrationsToDegrees.table");
 
         StructType teachersSchema =  new StructType(new StructField[]{
                 //TeacherId, TeacherName, Address, BirthyearTeacher, Gender
@@ -62,7 +59,7 @@ public class Q1 {
                 new StructField("BirthyearTeacher", DataTypes.IntegerType, true, Metadata.empty()),
                 new StructField("Gender", DataTypes.StringType, true, Metadata.empty())
         });
-        Dataset<Row> dfTeachers = spark.read().schema(teachersSchema).option("header", true).csv(startingpath+"Teachers.table");
+        Dataset<Row> dfTeachers = spark.read().schema(teachersSchema).option("header", true).csv(STARTING_PATH +"Teachers.table");
 
         StructType coursesSchema =  new StructType(new StructField[]{
                 //CourseId, CourseName, CourseDescription, DegreeId, ECTS
@@ -72,7 +69,7 @@ public class Q1 {
                 new StructField("DegreeId", DataTypes.IntegerType, true, Metadata.empty()),
                 new StructField("ECTS", DataTypes.IntegerType, true, Metadata.empty())
         });
-        Dataset<Row> dfCourses = spark.read().schema(coursesSchema).option("header", true).csv(startingpath+"Courses.table");
+        Dataset<Row> dfCourses = spark.read().schema(coursesSchema).option("header", true).csv(STARTING_PATH +"Courses.table");
 
         StructType courseOfferSchema =  new StructType(new StructField[]{
                 //CourseOfferId, CourseId, Year, Quartile
@@ -82,7 +79,7 @@ public class Q1 {
                 new StructField("Quartile", DataTypes.IntegerType, true, Metadata.empty())
         });
         Dataset<Row> dfCourseOffers = spark.read().schema(courseOfferSchema).option("header", true)
-                .csv(startingpath+"CourseOffers.table");
+                .csv(STARTING_PATH +"CourseOffers.table");
 
         StructType courseRegSchema =  new StructType(new StructField[]{
                 //CourseOfferId, StudentRegistrationId, Grade
@@ -92,7 +89,7 @@ public class Q1 {
         });
         Dataset<Row> dfCourseRegistrations = spark.read().schema(courseRegSchema).option("header", true)
                 .option("nullValue", "null")
-                .csv(startingpath+"CourseRegistrations.table");
+                .csv(STARTING_PATH +"CourseRegistrations.table");
 
         try {
             dfCourseOffers.createTempView("courseOffers");
@@ -106,7 +103,8 @@ public class Q1 {
                 "GROUP BY co.CourseOfferId");
         q11.show();
 
-        Dataset<Row> q12 = spark.sql("");
+        Dataset<Row> q12 = spark.sql("SELECT AVG(cr.grade) FROM courseRegistrations as cr " +
+                "WHERE cr.grade > 4 AND cr.StudentRegistrationId = 3 ");
         q12.show();
     }
 }
